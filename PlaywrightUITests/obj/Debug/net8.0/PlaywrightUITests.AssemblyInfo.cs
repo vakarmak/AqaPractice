b1d55ff842bd338992a26e2d8e33a1823a50b62b
Maksym Vakarchuk <maksym.vakarchuk@dataart.com>
2024-07-11 00:10:57 +0300

    test

--- PlaywrightUITests/obj/Debug/net8.0/PlaywrightUITests.AssemblyInfo.cs
+++ PlaywrightUITests/obj/Debug/net8.0/PlaywrightUITests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightUITests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35da8addd4691a490cfc69c294218411d0d8592")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884c2d5e60a35274ba5e02c41f9e515a06ff76da")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightUITests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightUITests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
