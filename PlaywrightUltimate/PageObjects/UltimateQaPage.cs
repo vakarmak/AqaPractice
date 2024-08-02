de7b597d982fd23a91cda0b851f54dc2c5acba10
Maksym Vakarchuk <vakarmak@gmail.com>
2024-08-02 18:16:23 +0300

    initial project setup

---
+++ PlaywrightUltimate/PageObjects/UltimateQaPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace PlaywrightUltimate.PageObjects
+{
+    internal class UltimateQaPage(IPage page)
+    {
+        private const string UltimateQaTargetPage = "https://courses.ultimateqa.com/enrollments";
+
+        // Locators
+
+
+
+        // Methods
+
+        public async Task GoToUltimateQaPage()
+        {
+            await page.GotoAsync(UltimateQaTargetPage);
+        }
+    }
+}
---
+++ PlaywrightUltimate/Tests/UltimateQaTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlaywrightUltimate.PageObjects;
+
+namespace PlaywrightUltimate.Tests
+{
+    internal class UltimateQaTests : UiTestFixture
+    {
+        private UltimateQaPage _ultimateQaPage;
+
+        [SetUp]
+        public void SetupUltimateQaPage()
+        {
+            _ultimateQaPage = new UltimateQaPage(Page);
+        }
+
+        [Test]
+        public async Task GoToUltimateQaPage()
+        {
+            await _ultimateQaPage.GoToUltimateQaPage();
+        }
+    }
+}
