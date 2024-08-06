9b53ef6bfc338fd390cf2df6a1b6efe7b6cd7755
Maksym Vakarchuk <vakarmak@gmail.com>
2024-08-07 01:08:48 +0300

    Homework 21 (#19)
    * transfer homework from repository
    
    * final implementation
    
    * transfer homework
    
    * final implementation

---
+++ PlaywrightUITests/Homework21/InterfaceHomework.cs
@@ -0,0 +1,83 @@
+﻿namespace PlaywrightUiTests.Homework21;
+
+public interface IMyWebDriver
+{
+    public void Open(string url);
+    public void FindElement(string locator);
+    public void Close();
+}
+
+public interface IMyWindowsWebDriver
+{
+    public string GetWindowsVersion();
+}
+
+public class ChromeDriver : IMyWebDriver, IMyWindowsWebDriver
+{
+    private const string DriverName = "Chrome";
+
+    public void Open(string url)
+    {
+        Console.WriteLine($"Opening {DriverName}");
+    }
+
+    public void FindElement(string locator)
+    {
+        Console.WriteLine($"Find {locator}");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine($"Closing {DriverName}");
+    }
+
+    public string GetWindowsVersion()
+    {
+        return "Windows 10";
+    }
+}
+
+public class SafariDriver : IMyWebDriver
+{
+    private const string DriverName = "Safari";
+
+    public void Open(string url)
+    {
+        Console.WriteLine($"Opening {DriverName}");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine($"Closing {DriverName}");
+    }
+
+    public void FindElement(string locator)
+    {
+        Console.WriteLine($"Find {locator}");
+    }
+}
+
+public class FirefoxDriver : IMyWebDriver, IMyWindowsWebDriver
+{
+    private const string DriverName = "Firefox";
+
+    public void FindElement(string locator)
+    {
+        Console.WriteLine($"Find {locator}");
+    }
+
+    public void Open(string url)
+    {
+        Console.WriteLine($"Opening {DriverName}");
+    }
+
+    public string GetWindowsVersion()
+    {
+        return "Windows 11";
+    }
+
+    public void Close()
+    {
+        Console.WriteLine($"Closing {DriverName}");
+    }
+}
