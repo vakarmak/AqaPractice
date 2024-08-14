24dd2d6a13a7607985cbd43c8adc9e08e56c6bc2
Maksym Vakarchuk <vakarmak@gmail.com>
2024-08-14 21:25:23 +0300

    third part of homework

--- PlaywrightSolar/PageObjects/InvertersPage.cs
+++ PlaywrightSolar/PageObjects/InvertersPage.cs
@@ -30,7 +30,7 @@ public class InvertersPage(IPage page)
 
         var textNameOfSelectedProduct = await nameOfSelectedProduct!.InnerTextAsync();
         var productNameText = ExtractProductNameText(textNameOfSelectedProduct);
-        
+
         await Task.Delay(1000);
 
         var addToCartButton = await selectedProductFromPage.QuerySelectorAsync(".add-product-to-cart");
@@ -46,5 +46,41 @@ public class InvertersPage(IPage page)
 
         var match = regex.Match(text);
         return match.Success ? match.Value.Trim() : "Product name not found";
+    }
+
+    public async Task<string> GoToInverterDetailsPage()
+    {
+        var listOfProductsOnPage = await page.QuerySelectorAllAsync(".col.s12.m6.l4.xl3.prod-holder");
+        var random = new Random();
+        var randomIndex = random.Next(listOfProductsOnPage.Count);
+        var selectedProductFromPage = listOfProductsOnPage[randomIndex];
+
+        var nameOfSelectedProduct = await selectedProductFromPage.QuerySelectorAsync(".list-product-title");
+
+        var textNameOfSelectedProduct = await nameOfSelectedProduct!.InnerTextAsync();
+        var productNameText = ExtractProductNameText(textNameOfSelectedProduct);
+
+        await selectedProductFromPage!.ClickAsync();
+
+        return productNameText;
+    }
+
+    public async Task<string> GetProductDetailsName(string expectedProductName)
+    {
+        var productDetailsCard =
+            await page.QuerySelectorAsync("//div[@class='col s12 m6 l5 xl4 black-text right-block']");
+        var actualProductName = await productDetailsCard!.InnerTextAsync();
+        var actualProductNameText = ExtractActualProductName(actualProductName, expectedProductName);
+
+        return actualProductNameText;
+    }
+
+    private static string ExtractActualProductName(string text, string target)
+    {
+        var regex = new Regex($@"^.*?({Regex.Escape(target)}).*?$", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+        var match = regex.Match(text);
+
+        return match.Success ? match.Groups[1].Value.Trim() : "Product name not found";
     }
 }
---
+++ PlaywrightSolar/Tests/CompareProductNamesTest.cs
@@ -0,0 +1,30 @@
+﻿using PlaywrightSolar.PageObjects;
+
+namespace PlaywrightSolar.Tests;
+
+internal class CompareProductNamesTest : UiTestFixture
+{
+    private SolarShopPage _solarShopPage;
+    
+    [SetUp]
+    public void SetupSolarTechnologyShopPage()
+    {
+        _solarShopPage = new SolarShopPage(Page);
+    }
+    
+    [Test]
+    public async Task CompareProductNames()
+    {
+        // Arrange
+        await _solarShopPage.GoToSolarTechnologyShopPage();
+        await _solarShopPage.GoToInvertersPage();
+        var inverterPage = new InvertersPage(Page);
+        
+        // Act
+        var expectedProductName = await inverterPage.GoToInverterDetailsPage();
+        var actualProductName = await inverterPage.GetProductDetailsName(expectedProductName);
+
+        // Assert
+        Assert.That(expectedProductName, Is.EqualTo(actualProductName), "Product names are not the same");
+    }
+}
