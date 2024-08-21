0928796cc3f561ff0e7e9b041692a78f91ed320e
Maksym Vakarchuk <vakarmak@gmail.com>
2024-08-21 15:36:04 +0300

    Solar feature playwright homework 18 (#21)
    * Project initialization
    
    * First part of homework
    
    * fix solar panels page method
    
    * interim comment
    
    * second part of homework
    
    * third part of homework
    
    * methods names refactoring
    
    * processed comments
    
    * processed comments part 2
    
    * improvements
    
    * processed comments part 3

---
+++ PlaywrightSolar/PageObjects/SolarPanelsPage.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightSolar.PageObjects;
+
+public class SolarPanelsPage(IPage page)
+{
+    private const string SolarPanelsPageUrl = "https://solartechnology.com.ua/shop/solar-panels";
+    
+    // Locators
+    private ILocator PageTitle => page.Locator("//h1");
+
+    private ILocator ProductFilterButton => page.GetByText("Фільтр товарів");
+
+    private readonly Dictionary<string, ILocator> _filterLocators = new()
+    {
+        { "ABi-Solar", page.GetByText("Abi-Solar", new PageGetByTextOptions { Exact = true }) },
+        { "C&T", page.GetByText("C&T Solar", new PageGetByTextOptions { Exact = true }) },
+        { "JA Solar", page.GetByText("JA Solar", new PageGetByTextOptions { Exact = true }) },
+        { "Jinko Solar", page.GetByText("Jinko Solar", new PageGetByTextOptions { Exact = true }) },
+        { "SOLA", page.GetByText("SOLA", new PageGetByTextOptions { Exact = true }) },
+        { "Ulica Solar", page.GetByText("Ulica Solar", new PageGetByTextOptions { Exact = true }) },
+        { "Yingli Solar", page.GetByText("Yingli Solar", new PageGetByTextOptions { Exact = true }) }
+    };
+    
+    private readonly Dictionary<string, ILocator> _panelTypeFiltersLocator = new()
+    {
+        { "Монокристал", page.GetByText("Монокристал", new PageGetByTextOptions { Exact = true }) },
+        { "Полікристал", page.GetByText("Полікристал", new PageGetByTextOptions { Exact = true }) }
+    };
+
+    // Methods
+    public async Task GoToSolarPanelsPage()
+    {
+        await page.GotoAsync(SolarPanelsPageUrl);
+    }
+
+    public async Task SolarPanelsPageOpened()
+    {
+        await Assertions.Expect(PageTitle).ToBeVisibleAsync();
+    }
+
+    public async Task OpenProductFilter()
+    {
+        await ProductFilterButton.ClickAsync();
+        await Task.Delay(1000);
+    }
+    
+    public async Task<List<string>> GetProductsName()
+    {
+        await page.WaitForSelectorAsync(".prod-holder");
+        
+        var productsCardContent = await page.QuerySelectorAllAsync(".card-content");
+        var cardContentText = await Task.WhenAll(productsCardContent.Select(async result => await result.InnerTextAsync()));
+        
+        return cardContentText.ToList();
+    }
+    
+    public async Task FilterProductsByManufacturer(string manufacturer)
+    {
+        var manufacturerLocator = _filterLocators[manufacturer];
+        await manufacturerLocator.CheckAsync();
+        
+        await Task.Delay(2000); // Other waiters are not working, everything that is related to the selector does not fit because selector always available, and stays the same
+    }
+
+    public async Task FilterProductsByPanelType(string panelType)
+    {
+        var panelTypeLocator = _panelTypeFiltersLocator[panelType];
+        await panelTypeLocator.CheckAsync();
+        
+        await Task.Delay(2000);
+    }
+}
---
+++ PlaywrightSolar/Tests/CatalogFiltersTest.cs
@@ -0,0 +1,58 @@
+﻿using PlaywrightSolar.PageObjects;
+
+namespace PlaywrightSolar.Tests;
+
+internal class CatalogFiltersTest : UiTestFixture
+{
+    private SolarPanelsPage _solarPanelsPage;
+    
+    [SetUp]
+    public void SetupSolarPanelsPage()
+    {
+        _solarPanelsPage = new SolarPanelsPage(Page);
+    }
+    
+    [Test]
+    public async Task VerifyCatalogFilteringByManufacturer()
+    {
+        // Arrange
+        await _solarPanelsPage.GoToSolarPanelsPage();
+        await _solarPanelsPage.SolarPanelsPageOpened();
+        await _solarPanelsPage.OpenProductFilter();
+        
+        // Act
+        // Possible values: "ABi-Solar", "C&T", "JA Solar", "Jinko Solar", "SOLA", "Ulica Solar", "Yingli Solar"
+        // var productsNameBeforeFiltering = await _solarPanelsPage.GetProductsName();
+        await _solarPanelsPage.FilterProductsByManufacturer("JA Solar");
+        var productsNameList = await _solarPanelsPage.GetProductsName();
+
+        // Assert
+        Assert.That(productsNameList, Is.Not.Empty, "Products list is empty");
+        foreach (var item in productsNameList)
+        {
+            Assert.That(item, Does.Contain("JA Solar"), "Filtered products do not contain selected manufacturer");
+        }
+
+    }
+    
+    [Test]
+    public async Task VerifyCatalogFilteringByPanelType()
+    {
+        // Arrange
+        await _solarPanelsPage.GoToSolarPanelsPage();
+        await _solarPanelsPage.SolarPanelsPageOpened();
+        await _solarPanelsPage.OpenProductFilter();
+        
+        // Act
+        // Possible values: "Монокристал", "Полікристал"
+        await _solarPanelsPage.FilterProductsByPanelType("Монокристал");
+        var productsNameByPanelType = await _solarPanelsPage.GetProductsName();
+        
+        // Assert
+        Assert.That(productsNameByPanelType, Is.Not.Empty, "Products are not found");
+        foreach (var item in productsNameByPanelType)
+        {
+            Assert.That(item, Does.Contain("Монокристал"), "Product does not contain selected panel type");
+        }
+    }
+}
