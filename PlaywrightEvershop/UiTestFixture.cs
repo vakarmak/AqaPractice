ecf41e009b04bf7690fdb14be7cd25f6531e980b
Maksym Vakarchuk <vakarmak@gmail.com>
2024-08-22 13:07:24 +0300

    project initialization

---
+++ PlaywrightEvershop/UiTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightEvershop;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+internal class UiTestFixture
+{
+    private IPage Page { get; set; }
+    private IBrowser _browser;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        var playwrightDriver = await Playwright.CreateAsync();
+        _browser = await playwrightDriver.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false, // Set false to run the browser in non-headless mode
+            Args = ["--start-maximized"] // Set the browser to start maximized
+        });
+
+        var context = await _browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            ViewportSize = ViewportSize.NoViewport
+        });
+        Page = await context.NewPageAsync();
+    }
+
+    [TearDown]
+    public async Task Teardown()
+    {
+        await Page.CloseAsync();
+        await _browser.CloseAsync();
+    }
+}
